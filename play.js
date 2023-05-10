e01c5bdd8ebce57159ea0ceea10067befa50f90c
@syabdulr <abdulsyed1998@gmail.com>
2023-05-10 16:48:58 -0400

    connect to server

--- play.js
+++ play.js
@@ -1,1 +1,17 @@
-//test 
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541'
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
